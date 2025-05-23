62ea6f50d7315606065cc43e634181eb5e1dd5aa
Leighton Welby <lwelby27@gmail.com>
2025-05-23 10:39:58 +0100

    Randmoising Number
    created a function to randomise a number between 0 and 1 to assign as computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 console.log("Hello World!")
+
+/*
+Pseudocode 
+- variable to store computer choice
+- function to randomly choose one of the 3 options
+- update variable to contain the selected choice
+*/
+
+
+let computerChoice;
+
+function getComputerChoice(){
+    computerChoice = Math.random();
+}
+
+getComputerChoice();
+console.log(computerChoice);
+
